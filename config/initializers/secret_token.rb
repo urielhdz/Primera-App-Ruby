6502fb27e322b8bf67d8cea8e1ecf80c5495920d
Uriel Hernandez <projectdxv@gmail.com>
2012-04-27 12:10:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReailsinstallerDemo::Application.config.secret_token = '61e247a74a7c8ad1d158d9126586cbd797d60b5dc7fb90a417a3a5025f937a5ea9ca3ae74eafaff8341e3488d75dcdbf6aefdc97caf427c7201044754327b2e8'
